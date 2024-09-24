7642af630f32fba24385e500fe1b2d2150bde238
Hiroshi Atsuta <atsuta@ieee.org>
2024-09-24 20:59:43 +0900

    Add data_handling.py

--- src/affetto_nn_ctrl/__init__.py
+++ src/affetto_nn_ctrl/__init__.py
@@ -1,2 +1,18 @@
-def hello() -> str:
-    return "Hello from affetto-nn-ctrl!"
+from __future__ import annotations
+
+from pathlib import Path
+
+ROOT_DIR_PATH = Path(__file__).parent.parent.parent
+SRC_DIR_PATH = ROOT_DIR_PATH / "src"
+APPS_DIR_PATH = ROOT_DIR_PATH / "apps"
+DEFAULT_BASE_DIR_PATH = ROOT_DIR_PATH / "data"
+DEFAULT_CONFIG_PATH = ROOT_DIR_PATH / "config" / "affetto.toml"
+
+DEFAULT_DURATION = 10.0  # sec
+DEFAULT_SEED = None
+DEFAULT_N_REPEAT = 1
+DEFAULT_TIME_HOME = 10
+
+# Local Variables:
+# jinx-local-words: "src"
+# End:
---
+++ src/affetto_nn_ctrl/data_handling.py
@@ -0,0 +1,71 @@
+from __future__ import annotations
+
+import datetime
+import itertools
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+from affetto_nn_ctrl import APPS_DIR_PATH, DEFAULT_BASE_DIR_PATH
+
+if TYPE_CHECKING:
+    from collections.abc import Iterator
+
+
+def get_default_base_dir() -> Path:
+    base_dir_config = APPS_DIR_PATH / "base_dir"
+    if base_dir_config.exists():
+        return Path(base_dir_config.read_text())
+    return DEFAULT_BASE_DIR_PATH
+
+
+def get_default_counter(start: int = 0, step: int = 1, fmt: str = "{:03d}") -> Iterator:
+    return map(fmt.format, itertools.count(start, step))
+
+
+def get_data_path(
+    base_dir: str | Path | None = None,
+    app_name: str = "app",
+    label: str = "test",
+    sublabel: str | None = None,
+    ext: str | None = None,
+    prefix: str = "",
+    iterator: Iterator | None = None,
+    *,
+    split_by_date: bool = True,
+    millisecond: bool = False,
+) -> Path:
+    if base_dir is None:
+        base_dir = DEFAULT_BASE_DIR_PATH
+
+    # Build upon the provided app name and label.
+    built_path: Path = Path(base_dir) / app_name / label
+
+    # Split directory into sub-directory by date.
+    if split_by_date:
+        fmt = "%Y%m%dT%H%M%S"
+        if millisecond:
+            fmt += ".%f"
+        now = datetime.datetime.now().strftime(fmt)  # noqa: DTZ005
+        built_path /= now
+
+    # Add the provided sublabel.
+    if sublabel is not None and len(sublabel) > 0:
+        built_path /= sublabel
+
+    # When ext is provided, generate a specific filename with prefix and iterator.
+    if ext is not None:
+        if len(ext) > 0:
+            ext = f".{ext}" if not ext.startswith(".") else ext
+        suffix = next(iterator) if iterator else ""
+        filename = f"{prefix}{suffix}{ext}"
+        if len(filename) == len(ext):
+            msg = "Extension was given, but unable to determine filename"
+            raise ValueError(msg)
+        built_path /= filename
+
+    return built_path
+
+
+# Local Variables:
+# jinx-local-words: "dT dir noqa sublabel"
+# End:
