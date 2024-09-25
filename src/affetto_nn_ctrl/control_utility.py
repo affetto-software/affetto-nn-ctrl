5736aad6f3535671e74b0a97b43bbdff5b1dffce
Hiroshi Atsuta <atsuta@ieee.org>
2024-09-25 21:33:47 +0900

    Update control_utility.py

--- src/affetto_nn_ctrl/control_utility.py
+++ src/affetto_nn_ctrl/control_utility.py
@@ -1,18 +1,19 @@
+# ruff: noqa: S311
 from __future__ import annotations
 
-import os
 import random
+import sys
 import time
-import warnings
-from datetime import datetime
-from pathlib import Path
-from typing import TYPE_CHECKING, TypeVar
+from typing import TYPE_CHECKING
 
 import numpy as np
 from affctrllib import PTP, AffComm, AffPosCtrl, AffStateThread, Logger, Timer
 
+from affetto_nn_ctrl.event_logging import get_event_logger
+
 if TYPE_CHECKING:
-    from typing import Callable, Iterable
+    from collections.abc import Callable
+    from pathlib import Path
 
 
 def set_seed(seed: int | None) -> None:
@@ -20,51 +21,41 @@ def set_seed(seed: int | None) -> None:
     np.random.seed(seed)  # noqa: NPY002
 
 
-T = TypeVar("T")
+EPSILON = 1e-4
 
 
-def prepare_ctrl(
+def create_controller(
     config: str,
-    srate: float | None,
-    crate: float | None,
+    sfreq: float | None,
+    cfreq: float | None,
     waiting_time: float = 5.0,
 ) -> tuple[AffComm, AffPosCtrl, AffStateThread]:
+    event_logger = get_event_logger()
+    if event_logger:
+        msg = f"Loaded config: {config}"
+        event_logger.info(msg)
+        msg = f"sensor frequency: {sfreq}, control frequency: {cfreq}"
+        event_logger.debug(msg)
+
     comm = AffComm(config_path=config)
     comm.create_command_socket()
-    state = AffStateThread(config=config, freq=srate, logging=False, output=None, butterworth=True)
-    ctrl = AffPosCtrl(config_path=config, freq=crate)
+    state = AffStateThread(config=config, freq=sfreq, logging=False, output=None, butterworth=True)
+    ctrl = AffPosCtrl(config_path=config, freq=cfreq)
     state.prepare()
     state.start()
-    print("Waiting until robot gets stationary...")  # noqa: T201
+    if event_logger:
+        event_logger.debug("Controller created.")
+
+    if event_logger:
+        msg = f"Waiting until robot gets stationary for {waiting_time} s..."
+        event_logger.info(msg)
     time.sleep(waiting_time)
+
     return comm, ctrl, state
 
 
-def get_datetime(_format: str = "%Y%m%dT%H%M%S") -> str:
-    return datetime.now().strftime(_format)  # noqa: DTZ005
-
-
-def prepare_output_directory(output_directory: str | Path | None, make_backup: bool = False) -> Path | None:
-    if output_directory is None:
-        warnings.warn(f"Output directory is not provdided. No data will be saved.")
-        return
-
-    path = Path(output_directory)
-    if make_backup:
-        symlink = path / "latest"
-        path = path / get_datetime()
-        if not symlink.exists() or symlink.is_symlink():
-            try:
-                os.remove(symlink)
-            except OSError:
-                pass
-            finally:
-                os.symlink(path.absolute(), symlink)
-    path.mkdir(parents=True, exist_ok=True)
-    return path
-
-
-def prepare_default_logger(dof: int) -> Logger:
+def create_default_logger(dof: int) -> Logger:
+    event_logger = get_event_logger()
     logger = Logger()
     logger.set_labels(
         "t",
@@ -84,6 +75,9 @@ def prepare_default_logger(dof: int) -> Logger:
         [f"qdes{i}" for i in range(dof)],
         [f"dqdes{i}" for i in range(dof)],
     )
+    if event_logger:
+        event_logger.debug("Default logger created.")
+
     return logger
 
 
@@ -99,10 +93,23 @@ def create_const_trajectory(
         return q
 
     def dqdes_func(_: float) -> np.ndarray:
-        dq = np.zeros((len(q0),))
-        return dq
+        return np.zeros((len(q0),))
 
     return qdes_func, dqdes_func
+
+
+def _reset_logger(logger: Logger | None, log_filename: str | Path | None) -> Logger | None:
+    event_logger = get_event_logger()
+    if logger is not None:
+        logger.erase_data()
+        if event_logger:
+            event_logger.debug("Logger data has been erased.")
+        if log_filename is not None:
+            logger.set_filename(log_filename)
+            if event_logger:
+                msg = f"Logger filename is updated: {log_filename}"
+                event_logger.debug(msg)
+    return logger
 
 
 def control_position(
@@ -113,16 +120,17 @@ def control_position(
     dqdes_func: Callable[[float], np.ndarray | float],
     duration: float,
     logger: Logger | None = None,
+    log_filename: str | Path | None = None,
     header_text: str = "",
 ) -> tuple[np.ndarray, np.ndarray]:
-    timer = Timer(rate=ctrl.freq)
-    if logger is not None:
-        logger.erase_data()
-    timer.start()
-    t = 0
+    _reset_logger(logger, log_filename)
     ca, cb = np.zeros((ctrl.dof,)), np.zeros((ctrl.dof,))
+    timer = Timer(rate=ctrl.freq)
+
+    timer.start()
+    t = 0.0
     while t < duration:
-        print(f"\r{header_text} [{t:6.2f}/{duration:.2f}]", end="")  # noqa: T201
+        sys.stdout.write(f"\r{header_text} [{t:6.2f}/{duration:.2f}]")
         t = timer.elapsed_time()
         rq, rdq, rpa, rpb = state.get_raw_states()
         q, dq, pa, pb = state.get_states()
@@ -133,30 +141,8 @@ def control_position(
         if logger is not None:
             logger.store(t, rq, rdq, rpa, rpb, q, dq, pa, pb, ca, cb, qdes, dqdes)
         timer.block()
-    # return the last command sent to the valve
-    print("")  # noqa: T201
-    return ca, cb
-
-
-def getbackhome_position(
-    comm: AffComm,
-    ctrl: AffPosCtrl,
-    state: AffStateThread,
-    qhome: np.ndarray,
-    duration: float,
-) -> tuple[np.ndarray, np.ndarray]:
-    q0 = state.q
-    ptp = PTP(q0, qhome, duration)
-    qdes_func, dqdes_func = ptp.q, ptp.dq
-    ca, cb = control_position(
-        comm,
-        ctrl,
-        state,
-        qdes_func,
-        dqdes_func,
-        duration,
-        header_text=f"Getting back to home position...",
-    )
+    sys.stdout.write("\n")
+    # Return the last commands that have been sent to the valve.
     return ca, cb
 
 
@@ -168,18 +154,18 @@ def control_valve(
     cb_func: Callable[[float], np.ndarray | float],
     duration: float,
     logger: Logger | None = None,
+    log_filename: str | Path | None = None,
     header_text: str = "",
 ) -> tuple[np.ndarray, np.ndarray]:
+    _reset_logger(logger, log_filename)
+    ca, cb = np.zeros((ctrl.dof,)), np.zeros((ctrl.dof,))
     timer = Timer(rate=ctrl.freq)
-    if logger is not None:
-        logger.erase_data()
     dummy = np.asarray([-1.0 for _ in range(ctrl.dof)])
 
     timer.start()
-    t = 0
-    ca, cb = np.zeros((ctrl.dof,)), np.zeros((ctrl.dof,))
+    t = 0.0
     while t < duration:
-        print(f"\r{header_text} [{t:6.2f}/{duration:.2f}]", end="")  # noqa: T201
+        sys.stdout.write(f"\r{header_text} [{t:6.2f}/{duration:.2f}]")
         t = timer.elapsed_time()
         rq, rdq, rpa, rpb = state.get_raw_states()
         q, dq, pa, pb = state.get_states()
@@ -189,74 +175,50 @@ def control_valve(
         if logger is not None:
             logger.store(t, rq, rdq, rpa, rpb, q, dq, pa, pb, ca, cb, dummy, dummy)
         timer.block()
-    # return the last command sent to the valve
-    print("")  # noqa: T201
+    sys.stdout.write("\n")
+    # Return the last commands that have been sent to the valve.
     return ca, cb
 
 
-def getbackhome_valve(
+def get_back_home_position(
     comm: AffComm,
     ctrl: AffPosCtrl,
     state: AffStateThread,
-    cahome: np.ndarray,
-    cbhome: np.ndarray,
+    q_home: np.ndarray,
     duration: float,
 ) -> tuple[np.ndarray, np.ndarray]:
+    event_logger = get_event_logger()
+    q0 = state.q
+    ptp = PTP(q0, q_home, duration)
+    qdes_func, dqdes_func = ptp.q, ptp.dq
+    msg = "Getting back to home position..."
+    if event_logger:
+        event_logger.debug(msg)
+    ca, cb = control_position(comm, ctrl, state, qdes_func, dqdes_func, duration, header_text=msg)
+    if event_logger:
+        event_logger.debug("Done")
+    return ca, cb
+
+
+def get_back_home_valve(
+    comm: AffComm,
+    ctrl: AffPosCtrl,
+    state: AffStateThread,
+    ca_home: np.ndarray,
+    cb_home: np.ndarray,
+    duration: float,
+) -> tuple[np.ndarray, np.ndarray]:
+    event_logger = get_event_logger()
     ca0, cb0 = np.zeros((ctrl.dof,)), np.zeros((ctrl.dof,))
-    # ca0, cb0 = state.pa, state.pb
-    ptp_ca = PTP(ca0, cahome, duration, profile_name="const")
-    ptp_cb = PTP(cb0, cbhome, duration, profile_name="const")
+    ptp_ca = PTP(ca0, ca_home, duration, profile_name="const")
+    ptp_cb = PTP(cb0, cb_home, duration, profile_name="const")
     ca_func, cb_func = ptp_ca.q, ptp_cb.q
-    ca, cb = control_valve(
-        comm,
-        ctrl,
-        state,
-        ca_func,
-        cb_func,
-        duration,
-        header_text=f"Getting back to home position (by valve)...",
-    )
-    return ca, cb
-
-
-def control_command(
-    comm: AffComm,
-    ctrl: AffPosCtrl,
-    state: AffStateThread,
-    commands: CommandSet,
-    duration: float,
-    seed: int | None,
-    logger: Logger | None = None,
-    header_text: str = "",
-) -> tuple[np.ndarray, np.ndarray]:
-    timer = Timer(rate=ctrl.freq)
-    if logger is not None:
-        logger.erase_data()
-    dummy = np.asarray([-1.0 for _ in range(ctrl.dof)])
-
-    # reset commands
-    set_seed(seed)
-    commands.reset()
-
-    t = 0
-    cnt = 0
-    ca, cb = np.zeros((ctrl.dof,)), np.zeros((ctrl.dof,))
-    timer.start()
-    while t < duration:
-        print(f"\r{header_text} [{t:6.2f}/{duration:.2f}]", end="")  # noqa: T201
-        # t = timer.elapsed_time()
-        t = timer.period * cnt
-        rq, rdq, rpa, rpb = state.get_raw_states()
-        q, dq, pa, pb = state.get_states()
-        ca, cb = commands.update(t, q, dq)
-        # ca, cb = commands.update(t, rq, rdq)
-        comm.send_commands(ca, cb)
-        if logger is not None:
-            logger.store(t, rq, rdq, rpa, rpb, q, dq, pa, pb, ca, cb, dummy, dummy)
-        timer.block()
-        cnt += 1
-    print("")  # noqa: T201
-    # return the last command sent to the valve
+    msg = "Getting back to home position (by valve)..."
+    if event_logger:
+        event_logger.debug(msg)
+    ca, cb = control_valve(comm, ctrl, state, ca_func, cb_func, duration, header_text=msg)
+    if event_logger:
+        event_logger.debug("Done")
     return ca, cb
 
 
@@ -294,9 +256,7 @@ class RandomTrajectory:
                 # Reduce limits of waist joint.
                 self.q_limits[i] = (40.0, 60.0)
         self.profile = profile
-        if seed is not None:
-            random.seed(seed)
-            np.random.seed(seed)
+        set_seed(seed)
         self.ptp_list = self.initialize_ptp()
 
     def _get_new_T_qdes(
@@ -318,7 +278,7 @@ class RandomTrajectory:
             elif qdes > qmax:
                 qdes = qmax - (qdes - qmax)
             qdes = max(min(qmax, qdes), qmin)
-            if abs(qdes - q0) > 0.0001:
+            if abs(qdes - q0) > EPSILON:
                 ok = True
         return T, qdes
 
@@ -357,5 +317,5 @@ class RandomTrajectory:
 
 
 # Local Variables:
-# jinx-local-words: "cb dT dq dqdes noqa pb qdes rdq rpa rpb rq"
+# jinx-local-words: "cb const dT dq dqdes noqa pb qdes rdq rpa rpb rq"
 # End:
