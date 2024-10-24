6a838f1794ba4c2a96486e73dadb55ac2b22af1b
Hiroshi Atsuta <atsuta@ieee.org>
2024-10-24 17:35:48 +0900

    Fix arguments of collect_data.py

--- apps/collect_data.py
+++ apps/collect_data.py
@@ -6,7 +6,6 @@ import argparse
 from pathlib import Path
 
 from affetto_nn_ctrl import DEFAULT_SEED
-from affetto_nn_ctrl.control_utility import GetGlobalRngT
 from affetto_nn_ctrl.data_handling import build_data_dir_path, get_default_base_dir, prepare_data_dir_path
 from affetto_nn_ctrl.event_logging import get_event_logger, start_event_logging
 
@@ -28,15 +27,14 @@ def run(
     q_range: tuple[float, float],
     q_limit: tuple[float, float],
     n_repeat: int,
+    seed: int | None,
     output_dir_path: Path,
-    seed: int | GetGlobalRngT | None,
     *,
     overwrite: bool,
 ) -> None:
     event_logger = get_event_logger()
     if event_logger:
-        msg = f"Config: {config}"
-        event_logger.debug(msg)
+        event_logger.debug("Config: %s", config)
 
 
 def make_output_dir(
@@ -44,6 +42,7 @@ def make_output_dir(
     output: str | None,
     label: str | None,
     sublabel: str | None,
+    specified_date: str | None,
     *,
     split_by_date: bool,
 ) -> Path:
@@ -58,6 +57,7 @@ def make_output_dir(
             APP_NAME_COLLECT_DATA,
             label,
             sublabel,
+            specified_date,
             split_by_date=split_by_date,
             millisecond=False,
         )
@@ -65,14 +65,16 @@ def make_output_dir(
 
 
 def parse() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description="Collect data by generating random trajectories of specified joints.")
+    parser = argparse.ArgumentParser(
+        description="Collect data by letting specified joints track randomly generated trajectories.",
+    )
     default_base_dir = get_default_base_dir()
     # Configuration
     parser.add_argument(
         "-b",
         "--base-dir",
         default=str(default_base_dir),
-        help="Base directory of data location.",
+        help="Base directory path for the current working project.",
     )
     parser.add_argument(
         "-c",
@@ -85,7 +87,7 @@ def parse() -> argparse.Namespace:
         "--joint",
         nargs="*",
         type=int,
-        help="Joint index list to make motion.",
+        help="Active joint index list to make motion.",
     )
     parser.add_argument(
         "-F",
@@ -108,7 +110,7 @@ def parse() -> argparse.Namespace:
         "--duration",
         default=DEFAULT_DURATION,
         type=float,
-        help="Time duration for each trajectory.",
+        help="Time duration of generated trajectory.",
     )
     parser.add_argument(
         "-s",
@@ -122,7 +124,7 @@ def parse() -> argparse.Namespace:
         default=DEFAULT_UPDATE_T_RANGE,
         nargs="+",
         type=float,
-        help="Time range when updating each joint angle reference.",
+        help="Time range when updating joint angle references.",
     )
     parser.add_argument(
         "-q",
@@ -130,7 +132,7 @@ def parse() -> argparse.Namespace:
         default=DEFAULT_UPDATE_Q_RANGE,
         nargs="+",
         type=float,
-        help="Joint angle range when updating.",
+        help="Joint angle range when updating joint angle references.",
     )
     parser.add_argument(
         "-Q",
@@ -138,40 +140,47 @@ def parse() -> argparse.Namespace:
         default=DEFAULT_Q_LIMIT,
         nargs="+",
         type=float,
-        help="Joint angle limit.",
+        help="Joint angle limit when generating joint angle references.",
     )
     parser.add_argument(
         "-n",
         "--n-repeat",
         default=DEFAULT_N_REPEAT,
         type=int,
-        help="Number of iterations to generate reference trajectories.",
+        help="Number of iterations to generate trajectories.",
     )
     # Output
     parser.add_argument(
         "-o",
         "--output",
-        help="Output directory where collected data files are stored.",
+        help="Output directory where collected dataset is stored.",
     )
     parser.add_argument(
         "--overwrite",
         action=argparse.BooleanOptionalAction,
         default=False,
-        help="Boolean. If True, overwrite existing reference trajectories.",
+        help="Boolean. If True, restart counter of generated data files and overwrite existing data files.",
     )
     parser.add_argument(
         "--label",
-        help="Label string for generated data.",
+        default="testing",
+        help="Label name of the current dataset.",
     )
     parser.add_argument(
         "--sublabel",
-        help="Optional. Sublabel string for generated data.",
+        help="Optional. Sublabel string for the current dataset.",
     )
     parser.add_argument(
         "--split-by-date",
         action=argparse.BooleanOptionalAction,
         default=True,
-        help="Boolean. If True, split outputs by date.",
+        help="Boolean. If True, split generated dataset by date.",
+    )
+    parser.add_argument(
+        "--specify-date",
+        help="Specify date string like '20240123T123456' or 'latest'. When the date string is specified, "
+        "generated dataset will be stored in the specified date directory. When 'latest' is specified, "
+        "find the latest directory.",
     )
     parser.add_argument(
         "--make-latest-symlink",
@@ -180,8 +189,26 @@ def parse() -> argparse.Namespace:
         help="Boolean. If True, make a symbolic link to the latest.",
     )
     # Others
-    parser.add_argument("-v", "--verbose", action="count", default=0, help="Make console output verbose.")
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        default=0,
+        help="Enable verbose console output. -v provides additional info. -vv provides debug output.",
+    )
     return parser.parse_args()
+
+
+def start_logging(argv: list[str], output_dir: Path, verbose_count: int) -> None:
+    match verbose_count:
+        case 0:
+            logging_level = "WARNING"
+        case 1:
+            logging_level = "INFO"
+        case _:
+            logging_level = "DEBUG"
+
+    start_event_logging(argv, output_dir, name=__name__, logging_level=logging_level)
 
 
 def main() -> None:
@@ -190,26 +217,18 @@ def main() -> None:
     args = parse()
 
     # Prepare input/output
-    match args.verbose:
-        case 0:
-            logging_level = "WARNING"
-        case 1:
-            logging_level = "INFO"
-        case _:
-            logging_level = "DEBUG"
-
     output_dir = make_output_dir(
         args.base_dir,
         args.output,
         args.label,
         args.sublabel,
+        args.specify_date,
         split_by_date=args.split_by_date,
     )
-    start_event_logging(sys.argv, output_dir, name=__name__, logging_level=logging_level)
+    start_logging(sys.argv, output_dir, args.verbose)
     event_logger = get_event_logger()
     if event_logger:
-        msg = f"Output directory: {output_dir}"
-        event_logger.info(msg)
+        event_logger.info("Output directory: %s", output_dir)
     prepare_data_dir_path(output_dir, make_latest_symlink=args.make_latest_symlink, dry_run=True)
 
     # Start mainloop
@@ -222,11 +241,11 @@ def main() -> None:
         # input
         # parameters
         args.duration,
-        args.seed,
         args.t_range,
         args.q_range,
         args.q_limit,
         args.n_repeat,
+        args.seed,
         # output
         output_dir,
         overwrite=args.overwrite,
@@ -237,5 +256,5 @@ if __name__ == "__main__":
     main()
 
 # Local Variables:
-# jinx-local-words: "cfreq dataset dir env sfreq sublabel symlink usr"
+# jinx-local-words: "cfreq dataset dir env sfreq sublabel symlink usr vv"
 # End:
